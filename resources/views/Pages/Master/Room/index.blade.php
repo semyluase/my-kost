b613c0e6feae973666ce53851dce9b26873f565e
IT-Semy <semy@fukuryo.co.id>
2024-12-14 12:28:26 +0700

    fix:remove modal-dialog-scroll

--- resources/views/Pages/Master/Room/index.blade.php
+++ resources/views/Pages/Master/Room/index.blade.php
@@ -44,7 +44,7 @@
     </div>
 </div>
 <div class="modal modal-blur fade" id="modal-room" tabindex="-1" style="display: none;" aria-hidden="true">
-    <div class="modal-dialog modal-dialog-centered modal-dialog-scrollable" role="document">
+    <div class="modal-dialog modal-dialog-centered" role="document">
         <div class="modal-content">
             <div class="modal-header">
                 <h5 class="modal-title">Kamar</h5>
