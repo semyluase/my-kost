1bf1b640d32d5b7cdf1d8ca8ab95340fb1285142
IT-Semy <semy@fukuryo.co.id>
2025-02-15 10:15:46 +0700

    update:update app path

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -5,6 +5,9 @@ use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 
 return Application::configure(basePath: dirname(__DIR__))
+    ->registered(function ($app) {
+        $app->usePublicPath(path: env('APP_PUBLIC_PATH'));
+    })
     ->withRouting(
         web: __DIR__ . '/../routes/web.php',
         commands: __DIR__ . '/../routes/console.php',
