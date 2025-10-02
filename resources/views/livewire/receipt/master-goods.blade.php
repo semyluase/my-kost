c3bd19ed8297203d72774a17ea6c802d46f08a41
IT Semy <semy@fukuryo.co.id>
2025-10-02 20:52:49 +0700

    update: add master goods just super admin

--- resources/views/livewire/receipt/master-goods.blade.php
+++ resources/views/livewire/receipt/master-goods.blade.php
@@ -6,16 +6,18 @@
     <div class="card-header">
         <h3 class="card-title">Barang</h3>
         <div class="card-actions">
-            <a href="{{ url('') }}/masters" class="btn btn-primary btn-3">
-                <!-- Download SVG icon from http://tabler.io/icons/icon/plus -->
-                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
-                    stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
-                    class="icon icon-2">
-                    <path d="M12 5l0 14"></path>
-                    <path d="M5 12l14 0"></path>
-                </svg>
-                Tambah Barang
-            </a>
+            @if (auth()->user()->role->slug == 'super-admin')
+                <a href="{{ url('') }}/masters" class="btn btn-primary btn-3">
+                    <!-- Download SVG icon from http://tabler.io/icons/icon/plus -->
+                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
+                        stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
+                        class="icon icon-2">
+                        <path d="M12 5l0 14"></path>
+                        <path d="M5 12l14 0"></path>
+                    </svg>
+                    Tambah Barang
+                </a>
+            @endif
         </div>
     </div>
     <div class="card-body overflow-scroll">
