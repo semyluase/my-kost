bab3c74424346def7c1fa1d8131a1448d525cb58
IT Semy <semy@fukuryo.co.id>
2025-10-08 15:43:17 +0700

    update : remove delete room

--- app/Http/Controllers/RoomController.php
+++ app/Http/Controllers/RoomController.php
@@ -277,9 +277,6 @@ class RoomController extends Controller
                                     <a href="javascript:;" class="btn-action text-warning" title="Ubah Kamar" onclick="fnRoom.onEdit(\'' . $value->slug . '\')">
                                         <svg  xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  class="icon icon-tabler icons-tabler-outline icon-tabler-edit"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M7 7h-1a2 2 0 0 0 -2 2v9a2 2 0 0 0 2 2h9a2 2 0 0 0 2 -2v-1" /><path d="M20.385 6.585a2.1 2.1 0 0 0 -2.97 -2.97l-8.415 8.385v3h3l8.385 -8.415z" /><path d="M16 5l3 3" /></svg>
                                     </a>
-                                    <a href="javascript:;" class="btn-action text-danger" title="Hapus Kamar" onclick="fnRoom.onDelete(\'' . $value->slug . '\',\'' . csrf_token() . '\')">
-                                        <svg  xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  class="icon icon-tabler icons-tabler-outline icon-tabler-eraser"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M19 20h-10.5l-4.21 -4.3a1 1 0 0 1 0 -1.41l10 -10a1 1 0 0 1 1.41 0l5 5a1 1 0 0 1 0 1.41l-9.2 9.3" /><path d="M18 13.3l-6.3 -6.3" /></svg>
-                                    </a>
                                 </div>';
 
                     $status = '<span class="badge bg-blue-lt">Tersedia</span>';
