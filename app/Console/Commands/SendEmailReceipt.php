8e78b40a86b3fbd77ca92c3b0c86db3110d2527b
IT Semy <semy@fukuryo.co.id>
2025-10-09 18:43:02 +0700

    update: get image

--- app/Console/Commands/SendEmailReceipt.php
+++ app/Console/Commands/SendEmailReceipt.php
@@ -33,6 +33,12 @@ class SendEmailReceipt extends Command
             ->where('is_order', true)
             ->get();
 
+        $path = public_path('assets/image/Asset 1.png');
+        $imageData = file_get_contents($path);
+        $mimeType = mime_content_type($path);
+        $base64 = base64_encode($imageData);
+        $dataUri = 'data:' . $mimeType . ';base64,' . $base64;
+
         if ($dataEmail) {
             foreach ($dataEmail as $key => $value) {
                 $dataTransaction = TransactionHeader::where('nobukti', $value->no_invoice)
@@ -40,6 +46,7 @@ class SendEmailReceipt extends Command
 
                 $pdf = Pdf::loadView('Pages.Services.PDF.receiptEmailAttach', [
                     'data'  =>  $dataTransaction,
+                    'image' =>  $dataUri,
                 ]);
 
                 $filePath = public_path('assets/invoice/' . $dataTransaction->nobukti . '.pdf');
