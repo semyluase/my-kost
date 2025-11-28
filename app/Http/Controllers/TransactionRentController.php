85823c11497e551ac3c2e953e05f734e2c2e630c
IT Semy <semy@fukuryo.co.id>
2025-11-28 09:49:46 +0700

    update : pindah kamar sebelum jam 12

--- app/Http/Controllers/TransactionRentController.php
+++ app/Http/Controllers/TransactionRentController.php
@@ -363,8 +363,13 @@ class TransactionRentController extends Controller
                 } else {
                     $sisaSewa = intval($request->sisaDurasi);
                     $totalSewa = round(Carbon::parse($dataKamar->rent->start_date)->diffInDays(Carbon::parse($dataKamar->rent->end_date), true), 0);
-                    $kurangBayar = 0;
+                    $kurangBayar = round($sisaSewa * ($hargaKamarBaru - $hargaKamarLama), 0);
                     $pembulatan = 0;
+                    if (substr($kurangBayar, -3) < 500 && substr($kurangBayar, -3) > 0) {
+                        $pembulatan = 500 - substr($kurangBayar, -3);
+                    } elseif (substr($kurangBayar, -3) > 500 && substr($kurangBayar, -3) < 0) {
+                        $pembulatan = 1000 - substr($kurangBayar, -3);
+                    }
                 }
 
                 $updateDataKamar = [
