91996b9bc14eed28898e3d089e896ee9cdbccf94
IT Semy <semy@fukuryo.co.id>
2026-01-23 09:53:23 +0700

    update : total kamar pada dashboard di ambil dari start sewa dan end sewa

--- app/Models/Room.php
+++ app/Models/Room.php
@@ -45,7 +45,7 @@ class Room extends Model
 
     function rentToday()
     {
-        return $this->belongsTo(TransactionRent::class, 'id', 'room_id')->where("end_date", '>=', Carbon::now('Asia/Jakarta')->isoFormat("YYYY-MM-DD"))->where('is_change_room', false)->where('is_checkout_abnormal', false)->where('is_checkout_normal', false);
+        return $this->belongsTo(TransactionRent::class, 'id', 'room_id')->where("start_date", ">=", Carbon::now('Asia/Jakarta')->isoFormat("YYYY-MM-DD"))->where("end_date", "<=", Carbon::now('Asia/Jakarta')->isoFormat("YYYY-MM-DD"))->where('is_change_room', false)->where('is_checkout_abnormal', false)->where('is_checkout_normal', false);
     }
 
     function oldRent()
