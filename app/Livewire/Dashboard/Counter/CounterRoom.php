36b02eff3ece50a742b7c1ecfbb3cb93ae47e404
Semy Luase <semy@fukuryo.co.id>
2026-07-06 21:01:49 +0700

    update : add tabs on dashboard
    for data branch

--- app/Livewire/Dashboard/Counter/CounterRoom.php
+++ app/Livewire/Dashboard/Counter/CounterRoom.php
@@ -3,17 +3,35 @@
 namespace App\Livewire\Dashboard\Counter;
 
 use App\Models\Category;
+use App\Models\Home;
+use Illuminate\Support\Facades\Auth;
+use Livewire\Attributes\Computed;
 use Livewire\Component;
 
 class CounterRoom extends Component
 {
+    #[Computed()]
+    function listCategories()
+    {
+        return Category::where('is_active', true)
+            ->get();
+    }
+
+    #[Computed()]
+    function listBranch()
+    {
+        if (Auth::user()->role->slug == 'super-admin') {
+            return Home::where('is_active', true)
+                ->get();
+        }
+
+        return Home::where('is_active', true)
+            ->where('id', Auth::user()->home_id)
+            ->get();
+    }
+
     public function render()
     {
-        $categories = Category::where('is_active', true)
-            ->get();
-
-        return view('livewire.dashboard.counter.counter-room', [
-            'categories'    =>  $categories
-        ]);
+        return view('livewire.dashboard.counter.counter-room');
     }
 }
--- resources/views/livewire/dashboard/counter/counter-room.blade.php
+++ resources/views/livewire/dashboard/counter/counter-room.blade.php
@@ -2,57 +2,82 @@
     @php
         use App\Models\Room;
     @endphp
-    @foreach ($categories as $category)
-        @php
-            $rooms = Room::CountDataByCategory($category->id)->first();
+    <div class="col-md-12">
+        <div class="card">
+            <div class="card-header">
+                <ul class="nav nav-tabs card-header-tabs" data-bs-toggle="tabs" role="tablist">
+                    @foreach ($this->listBranch as $branch)
+                        <li class="nav-item" role="presentation">
+                            <a href="#branch-{{ $branch->id }}" class="nav-link {{ $loop->first ? 'active' : '' }}"
+                                data-bs-toggle="tab" aria-selected="true" role="tab">{{ $branch->name }}</a>
+                        </li>
+                    @endforeach
+                </ul>
+            </div>
+            <div class="card-body">
+                <div class="tab-content">
+                    @foreach ($this->listBranch as $branch)
+                        <div class="tab-pane {{ $loop->first ? 'active show' : '' }}" id="branch-{{ $branch->id }}"
+                            role="tabpanel">
+                            <h4>{{ $branch->name }}</h4>
+                            <div class="row mb-3">
+                                @foreach ($this->listCategories as $category)
+                                    @php
+                                        $rooms = Room::CountDataByCategory($category->id, $branch->id)->first();
 
-            $totalRooms = Room::where('category_id', $category->id)->count();
-
-            if (auth()->user()->role->slug != 'super-admin') {
-                $rooms = Room::CountDataByCategory($category->id, auth()->user()->home_id)->first();
-
-                $totalRooms = Room::where('category_id', $category->id)
-                    ->where('home_id', auth()->user()->home_id)
-                    ->count();
-            }
-        @endphp
-        <div class="col-sm-6 col-lg-3 mb-3">
-            <div class="card shadow-sm">
-                <a href="{{ url('') }}/transactions/rent-rooms" class="text-decoration-none text-dark">
-                    <div class="card-body">
-                        <div class="d-flex align-items-center">
-                            <div class="h4 text-dark">{{ $category->name }}</div>
-                            <div class="ms-auto lh-1">
-                                <div class="text-secondary">Hari ini</div>
+                                        $totalRooms = Room::where('category_id', $category->id)
+                                            ->where('home_id', $branch->id)
+                                            ->count();
+                                    @endphp
+                                    <div class="col-sm-6 col-lg-3 mb-3">
+                                        <div class="card shadow-sm">
+                                            <a href="{{ url('') }}/transactions/rent-rooms"
+                                                class="text-decoration-none text-dark">
+                                                <div class="card-body">
+                                                    <div class="d-flex align-items-center">
+                                                        <div class="h4 text-dark">{{ $category->name }}</div>
+                                                        <div class="ms-auto lh-1">
+                                                            <div class="text-secondary">Hari ini</div>
+                                                        </div>
+                                                    </div>
+                                                    <div class="h1 mb-3">
+                                                        @if ($rooms)
+                                                            {{ $rooms->total }}/{{ $totalRooms }}
+                                                        @else
+                                                            0/0
+                                                        @endif
+                                                    </div>
+                                                    <div class="progress progress-sm">
+                                                        @php
+                                                            $totalKamar = 0;
+                                                            $terisi = 0;
+                                                            $percent = 0;
+                                                            if ($rooms) {
+                                                                $totalKamar = $totalRooms;
+                                                                $terisi = $rooms->total;
+                                                                $percent =
+                                                                    $totalKamar > 0 ? ($terisi / $totalKamar) * 100 : 0;
+                                                            }
+                                                        @endphp
+                                                        <div class="progress-bar bg-success"
+                                                            style="width: {{ $percent }}%" role="progressbar"
+                                                            aria-valuenow="{{ $terisi }}" aria-valuemin="0"
+                                                            aria-valuemax="{{ $totalKamar }}"
+                                                            aria-label="{{ $percent }} Terisi">
+                                                            <span class="visually-hidden">{{ $percent }}%
+                                                                Terisi</span>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </a>
+                                        </div>
+                                    </div>
+                                @endforeach
                             </div>
                         </div>
-                        <div class="h1 mb-3">
-                            @if ($rooms)
-                                {{ $rooms->total }}/{{ $totalRooms }}
-                            @else
-                                0/0
-                            @endif
-                        </div>
-                        <div class="progress progress-sm">
-                            @php
-                                $totalKamar = 0;
-                                $terisi = 0;
-                                $percent = 0;
-                                if ($rooms) {
-                                    $totalKamar = $totalRooms;
-                                    $terisi = $rooms->total;
-                                    $percent = $totalKamar > 0 ? ($terisi / $totalKamar) * 100 : 0;
-                                }
-                            @endphp
-                            <div class="progress-bar bg-success" style="width: {{ $percent }}%" role="progressbar"
-                                aria-valuenow="{{ $terisi }}" aria-valuemin="0"
-                                aria-valuemax="{{ $totalKamar }}" aria-label="{{ $percent }} Terisi">
-                                <span class="visually-hidden">{{ $percent }}% Terisi</span>
-                            </div>
-                        </div>
-                    </div>
-                </a>
+                    @endforeach
+                </div>
             </div>
         </div>
-    @endforeach
+    </div>
 </div>
